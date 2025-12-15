62c7f41ed2727d3d383ceeaa5933fe4027c48580
harumaki4649 <83683593+harumaki4649@users.noreply.github.com>
2025-12-15 23:53:03 +0900

    Update module path for WASM file

--- neuroreversi_engine.js
+++ neuroreversi_engine.js
@@ -262,7 +262,7 @@ async function __wbg_init(module_or_path) {
     }
 
     if (typeof module_or_path === 'undefined') {
-        module_or_path = new URL('/neuro-reversi-pages/neuroreversi_engine_bg.wasm', import.meta.url);
+        module_or_path = new URL('neuroreversi_engine_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
